fc1290ebfaff1aaf7389db1cbff85d85be8e38f8
aliramazanyildirim <aliramazanyildirim@gmail.com>
2022-08-29 15:28:40 +0300

    NLog und NLog.Web.Asp.NetCore Bibliotheken hinzufügen

--- NLogBibliothek/Controllers/HomeController.cs
+++ NLogBibliothek/Controllers/HomeController.cs
@@ -8,36 +8,15 @@ namespace NLogBibliothek.Controllers
     {
         //1. Weg
 
-        //private readonly ILogger<HomeController> _logger;
+        private readonly ILogger<HomeController> _logger;
 
-        //public HomeController(ILogger<HomeController> logger)
-        //{
-        //    _logger = logger;
-        //}
-
-        //public IActionResult Index()
-        //{
-        //    _logger.LogTrace("Indexseite wurde eingegeben.-Trace");
-        //    _logger.LogDebug("Indexseite wurde eingegeben.-Debug");
-        //    _logger.LogInformation("Indexseite wurde eingegeben.");
-        //    _logger.LogWarning("Indexseite wurde eingegeben.-Warning");
-        //    _logger.LogError("Indexseite wurde eingegeben.-Error");
-        //    _logger.LogCritical("Indexseite wurde eingegeben.-Critical");
-        //    return View();
-        //}
-
-        //2.Weg wenn man einen benutzerdefinierten Name verwenden möchte
-        private readonly ILoggerFactory _loggerFactory;
-
-        public HomeController(ILoggerFactory loggerFactory)
+        public HomeController(ILogger<HomeController> logger)
         {
-            _loggerFactory = loggerFactory;
+            _logger = logger;
         }
 
-        
         public IActionResult Index()
         {
-            var _logger= _loggerFactory.CreateLogger("Logging Kategorien");
             _logger.LogTrace("Indexseite wurde eingegeben.-Trace");
             _logger.LogDebug("Indexseite wurde eingegeben.-Debug");
             _logger.LogInformation("Indexseite wurde eingegeben.");
@@ -46,6 +25,27 @@ namespace NLogBibliothek.Controllers
             _logger.LogCritical("Indexseite wurde eingegeben.-Critical");
             return View();
         }
+
+        //2.Weg wenn man einen benutzerdefinierten Name verwenden möchte
+        //private readonly ILoggerFactory _loggerFactory;
+
+        //public HomeController(ILoggerFactory loggerFactory)
+        //{
+        //    _loggerFactory = loggerFactory;
+        //}
+
+
+        //public IActionResult Index()
+        //{
+        //    var _logger= _loggerFactory.CreateLogger("Logging Kategorien");
+        //    _logger.LogTrace("Indexseite wurde eingegeben.-Trace");
+        //    _logger.LogDebug("Indexseite wurde eingegeben.-Debug");
+        //    _logger.LogInformation("Indexseite wurde eingegeben.");
+        //    _logger.LogWarning("Indexseite wurde eingegeben.-Warning");
+        //    _logger.LogError("Indexseite wurde eingegeben.-Error");
+        //    _logger.LogCritical("Indexseite wurde eingegeben.-Critical");
+        //    return View();
+        //}
 
         public IActionResult Privacy()
         {
--- NLogBibliothek/Program.cs
+++ NLogBibliothek/Program.cs
@@ -1,37 +1,61 @@
-var builder = WebApplication.CreateBuilder(args);
-//builder.Logging.ClearProviders();
-builder.Host.ConfigureLogging(logging =>
+using NLog;
+using NLog.Web;
+
+var logger = NLog.LogManager.Setup().LoadConfigurationFromAppSettings().GetCurrentClassLogger();
+logger.Debug("init main");
+try
 {
-    logging.ClearProviders();
-    logging.AddDebug();
-});
+    var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.Services.AddControllersWithViews();
+    //builder.Logging.ClearProviders();
+    //builder.Host.ConfigureLogging(logging =>
+    //{
+    //    logging.ClearProviders();
+    //    logging.AddDebug();
+    //});
 
-var app = builder.Build();
+    // Add services to the container.
+    builder.Services.AddControllersWithViews();
 
-//Die Protokollierung(Logging) kann ohne Abhängigkeitsinjektion(Dependency Injection) erfolgen, wie unten beschrieben 
-var logger = app.Services.GetRequiredService<ILogger<Program>>();
-logger.LogInformation("Die App wird hochgefahren...");
+    //NLog: Richten Sie NLog für die Abhängigkeitsinjektion ein
+    builder.Logging.ClearProviders();
+    builder.Host.UseNLog();
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    var app = builder.Build();
+
+    //Die Protokollierung(Logging) kann ohne Abhängigkeitsinjektion(Dependency Injection) erfolgen, wie unten beschrieben 
+    //var logger = app.Services.GetRequiredService<ILogger<Program>>();
+    //logger.LogInformation("Die App wird hochgefahren...");
+
+    // Configure the HTTP request pipeline.
+    if (!app.Environment.IsDevelopment())
+    {
+        app.UseExceptionHandler("/Home/Error");
+        // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+        app.UseHsts();
+    }
+
+    app.UseHttpsRedirection();
+    app.UseStaticFiles();
+
+    app.UseRouting();
+
+    app.UseAuthorization();
+
+    app.MapControllerRoute(
+        name: "default",
+        pattern: "{controller=Home}/{action=Index}/{id?}");
+
+    app.Run();
 }
-
-app.UseHttpsRedirection();
-app.UseStaticFiles();
-
-app.UseRouting();
-
-app.UseAuthorization();
-
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
-
-app.Run();
+catch (Exception exception)
+{
+    // NLog: catch setup errors
+    logger.Error(exception, "Programm wegen Ausnahme gestoppt");
+    throw;
+}
+finally
+{
+    // Ensure to flush and stop internal timers/threads before application-exit (Avoid segmentation fault on Linux)
+    NLog.LogManager.Shutdown();
+}
